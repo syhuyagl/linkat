8c19e5fc1adeb501b63229af42f4785249864362
syhuyagl <ho_sy_huy@allgrow-labo.jp>
2023-02-17 12:14:51 +0700

    faq

--- src/_public/assets/js/main.js
+++ src/_public/assets/js/main.js
@@ -7,4 +7,19 @@ $(document).ready(function () {
     prevArrow: $(".c-arrow__left"),
     nextArrow: $(".c-arrow__right"),
   });
+  $(".c-faq__question").click(function () {
+    var answer = $(this).parent().find(".c-faq__answer");
+    var icon_close = $(this).find('.c-faq__icon').attr("src");
+    var icon_open = icon_close.replace("_close.", "_open.");
+
+    if (answer.hasClass("c-faq__answer--expand")) {
+      answer.removeClass("c-faq__answer--expand");
+      $(this).find('.c-faq__icon').attr("src", icon_open);
+      answer.slideUp();
+    } else {
+      answer.addClass("c-faq__answer--expand");
+      $(this).find('.c-faq__icon').attr("src", icon_close);
+      answer.slideDown();
+    }
+  });
 });
