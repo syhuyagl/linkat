489b2dd499895d6fc7994e11be5b366f0a72d580
syhuyagl <ho_sy_huy@allgrow-labo.jp>
2023-02-22 09:57:18 +0700

    update

--- src/_public/assets/js/main.js
+++ src/_public/assets/js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   $(".c-use__slider").slick({
     speed: 1100,
     infinite: true,
-    autoplay: false,
+    autoplay: true,
     arrows: true,
     prevArrow: $(".c-arrow__left"),
     nextArrow: $(".c-arrow__right"),
